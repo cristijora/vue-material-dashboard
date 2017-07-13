84fda5c4919f69e64e8007845806f9fc972da79c
cristijora <joracristi@gmail.com>
2017-07-14 00:22:48 +0300

    Update tests

--- test/unit/specs/Overview.spec.js
+++ test/unit/specs/Overview.spec.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Overview from 'src/components/Dashboard/Views/Overview.vue'
+import Dashboard from 'src/components/Dashboard/Views/Dashboard.vue'
 import Chartist from 'chartist'
 
 function getRenderedComponent (Component, propsData) {
@@ -16,11 +16,11 @@ function getRenderedComponent (Component, propsData) {
 }
 
 describe('Overview.vue', () => {
-  it('should contain 4 stats cards and 1 charts card', () => {
-    var vm = getRenderedComponent(Overview);
+  it('should contain 4 stats cards and 3 charts card', () => {
+    var vm = getRenderedComponent(Dashboard);
     var statsCards = vm.$children.filter((child)=>child.$options.name==='stats-card').length;
     var chartCards = vm.$children.filter((child)=>child.$options.name==='chart-card').length;
     expect(statsCards).to.equal(4)
-    expect(chartCards).to.equal(1)
+    expect(chartCards).to.equal(3)
   })
 })
